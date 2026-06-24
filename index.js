0431c5f6d200279d4079ce2cd8e77c20ff07a35c
kivenemad538-collab <kivenemad538@gmail.com>
2026-06-24 17:45:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+const BOARDING_ROLE_ID = '1516991167982735451';
+const HIDE_COMMUNITY_ROLE_ID = '1516991502839054378';
+
+client.on('guildMemberUpdate', async (oldMember, newMember) => {
+  const hasBoarding = newMember.roles.cache.has(BOARDING_ROLE_ID);
+  const hasHide = newMember.roles.cache.has(HIDE_COMMUNITY_ROLE_ID);
+
+  if (hasBoarding && !hasHide) {
+    await newMember.roles.add(HIDE_COMMUNITY_ROLE_ID);
+  }
+
+  if (!hasBoarding && hasHide) {
+    await newMember.roles.remove(HIDE_COMMUNITY_ROLE_ID);
+  }
+});
+
+client.login(process.env.TOKEN);
